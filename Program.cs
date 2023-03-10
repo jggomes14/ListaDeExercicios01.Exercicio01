ffb7287e71b590f367c4583f1a11832938817510
jgabr <jgabr@DESKTOP-KB55J7U>
2023-03-10 16:59:33 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ListaDeExercicios01.Exercicio01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calcule o volume da caixa retangular");
+            decimal Comprimento;
+            decimal Largura;
+            decimal Altura;
+            
+            Console.ReadKey();
+            Console.WriteLine("Insira o Comprimento");
+            Comprimento = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Insira o Largura");
+            Largura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Insira o Altura");
+            Altura = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("O Volume da Caixa Retangular é " + (Comprimento * Largura * Altura) + "L" );
+
+
+
+        }
+    }
+}
